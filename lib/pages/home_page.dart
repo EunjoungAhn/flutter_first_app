d9471101103b6753f0ab9f97c46e6c71deda451b
EunjoungAhn <aej9300@gmail.com>
2022-11-22 20:04:17 +0900

    구조 페이지 함수로 정리

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,57 +32,62 @@ class _HomePageState extends State<HomePage> {
           body: _pages[_currentIndex],
           // 가운데 + 버튼 
           floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              // 새로운 레아이웃 페이지로 이동
-              Navigator.push(
-                context, MaterialPageRoute(builder: (context) => const AddPage()));
-            },
+            onPressed: _onAddMedicien,
             child: const Icon(CupertinoIcons.add),
           ),
           floatingActionButtonLocation:
             FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: BottomAppBar(
-            elevation: 0, //바텀 네이게이션 그림자 설정
-            child: Container(
-              height: kBottomNavigationBarHeight, //material 자체에서 네비게이션 높이를 지정해준 것
-              color: Colors.white,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround, // row 정렬
-                children: [
-                  CupertinoButton(
-                    child: Icon(
-                      CupertinoIcons.add,
-                      color: _currentIndex == 0
-                          ? AppColors.primaryColor
-                          : Colors.grey[350], 
-                    ), 
-                    onPressed: () {
-                      setState(() {
-                        //고정된 화면에서 페이지 이동
-                        _currentIndex = 0;
-                      });
-                    },
-                  ),
-                  CupertinoButton(
-                    child: Icon(
-                      CupertinoIcons.text_badge_checkmark,
-                      color: _currentIndex == 1
-                          ? AppColors.primaryColor
-                          : Colors.grey[350], 
-                      ), 
-                    onPressed: () {
-                      setState(() {
-                        //고정된 화면에서 페이지 이동
-                        _currentIndex = 1;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
+          bottomNavigationBar: _buildBottomAppBar(),
         ),
       ),
     );
   }
+
+  BottomAppBar _buildBottomAppBar() {
+    return BottomAppBar(
+          elevation: 0, //바텀 네이게이션 그림자 설정
+          child: Container(
+            height: kBottomNavigationBarHeight, //material 자체에서 네비게이션 높이를 지정해준 것
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // row 정렬
+              children: [
+                CupertinoButton(
+                  onPressed: () => _onCurrentPage(0),
+                  child: Icon(
+                    CupertinoIcons.add,
+                    color: _currentIndex == 0
+                        ? AppColors.primaryColor
+                        : Colors.grey[350], 
+                  ), 
+                ),
+                CupertinoButton(
+                  child: Icon(
+                    CupertinoIcons.text_badge_checkmark,
+                    color: _currentIndex == 1
+                        ? AppColors.primaryColor
+                        : Colors.grey[350], 
+                    ), 
+                  onPressed: () => _onCurrentPage(1),
+                ),
+              ],
+            ),
+          ),
+        );
+  }
+
+  void _onCurrentPage(int pageIndex) {
+    setState(() {
+      //고정된 화면에서 페이지 이동
+      _currentIndex = pageIndex;
+    });
+  }
+
+
+  void _onAddMedicien(){
+    // 새로운 레아이웃 페이지로 이동
+    Navigator.push(
+      context, MaterialPageRoute(builder: (context) => const AddPage()));
+  }
+
 }
