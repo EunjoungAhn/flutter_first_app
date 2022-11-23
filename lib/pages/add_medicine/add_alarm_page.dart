abd981836ff5522909c005c29133a26d008bf69d
EunjoungAhn <aej9300@gmail.com>
2022-11-23 16:06:21 +0900

    service에 with ChangeNotifier 을 달고 setState가 필요한 부분의 함수에 notifyListeners를 추가 이후 사용할 곳에 AnimatedBuilder에 넣어서 적용해 주어야 한다.

--- lib/pages/add_medicine/add_alarm_page.dart
+++ lib/pages/add_medicine/add_alarm_page.dart
@@ -3,13 +3,14 @@ import 'dart:io';
 import 'package:first_app/components/app_colors.dart';
 import 'package:first_app/components/app_constants.dart';
 import 'package:first_app/components/app_widgets.dart';
+import 'package:first_app/services/add_medicin_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'components/add_page_widget.dart';
 
-class AddAlarmPage extends StatefulWidget {
-  const AddAlarmPage({
+class AddAlarmPage extends StatelessWidget {
+  AddAlarmPage({
     required this.medicineImage,
     required this.medicineName,
     super.key
@@ -18,16 +19,7 @@ class AddAlarmPage extends StatefulWidget {
   final File? medicineImage;
   final String medicineName;
 
-  @override
-  State<AddAlarmPage> createState() => _AddAlarmPageState();
-}
-
-class _AddAlarmPageState extends State<AddAlarmPage> {
-  final _alarms = <String>{
-    '10:00',
-    '12:00',
-    '13:00',
-  };
+  final service = AddMedicineService();
 
   @override
   Widget build(BuildContext context) {
@@ -41,8 +33,13 @@ class _AddAlarmPageState extends State<AddAlarmPage> {
           ),
           const SizedBox(height: largeSpace),
           Expanded(// Expanded- 컬럼에서 내가 가지고 있는 영역을 다 그릴수 있는 
-            child: ListView(
-              children: alarmWidgets,
+            child: AnimatedBuilder(
+              animation: service,
+              builder: (context, _) {
+                return ListView(
+                  children: alarmWidgets,
+                );
+              }, 
             ),
           ),
       ],
@@ -60,26 +57,14 @@ class _AddAlarmPageState extends State<AddAlarmPage> {
     //위젯을 받는 childern을 만든다.
     final children = <Widget>[];
       children.addAll(
-        _alarms.map(
+        service.alarms.map(
           (alarmTime) => AlarmBox(
           time: alarmTime,
-          onPressMinus: () {  
-            setState(() {
-              _alarms.remove(alarmTime);
-            });
-          },
+          service: service,
         )),
       );
       children.add(AddAlarmButton(
-        onPressed: () {  
-          final now = DateTime.now();
-          //now.minute 으로 출력하면 03분으로 안나온다.
-          //그래서 DateTime을 포맷해서 출력한다.
-          final nowTime = DateFormat('HH:mm').format(now); 
-          setState(() {
-            _alarms.add(nowTime); 
-          });
-        },
+        service: service,
       ));
     return children;
   }
@@ -88,11 +73,11 @@ class _AddAlarmPageState extends State<AddAlarmPage> {
 // 반복되는 부분 위젯으로
 class AlarmBox extends StatelessWidget {
   const AlarmBox({
-    Key? key, required this.time, required this.onPressMinus,
+    Key? key, required this.time, required this.service,
   }) : super(key: key);
 
   final String time;
-  final VoidCallback onPressMinus;
+  final AddMedicineService service;
 
   @override
   Widget build(BuildContext context) {
@@ -103,7 +88,9 @@ class AlarmBox extends StatelessWidget {
       Expanded(
         flex: 1,// 공간 크기 지정
         //위젯 안에서 처리하는 것이 아닌 밖에서 onPressed 처리
-        child: IconButton(onPressed: onPressMinus, 
+        child: IconButton(onPressed: () {
+          service.removeAlarm(time);
+        }, 
         icon: const Icon(CupertinoIcons.minus_circle),),
       ),
       Expanded(
@@ -183,10 +170,10 @@ class TimePickerBottomSheet extends StatelessWidget {
 
 class AddAlarmButton extends StatelessWidget {
   const AddAlarmButton({
-    Key? key, required this.onPressed,
+    Key? key, required this.service,
   }) : super(key: key);
 
-  final VoidCallback onPressed;
+  final AddMedicineService service;
 
   @override
   Widget build(BuildContext context) {
@@ -194,7 +181,7 @@ class AddAlarmButton extends StatelessWidget {
       style: TextButton.styleFrom(
         padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6),
         textStyle: Theme.of(context).textTheme.subtitle1),
-      onPressed: onPressed,
+      onPressed: service.addNowAlarm,
       child: Row(
         children: const [
           Expanded(
---
+++ lib/services/add_medicin_service.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+
+//service에 with ChangeNotifier 을 달고 
+//setState가 필요한 부분의 함수에 notifyListeners를 추가
+//이후 사용할 곳에 AnimatedBuilder에 넣어서 적용해 주어야 한다.
+class AddMedicineService with ChangeNotifier{
+  final _alarms = <String>{
+    '10:00',
+    '12:00',
+    '13:00',
+  };
+
+  Set<String> get alarms => _alarms;
+
+  void addNowAlarm(){
+    final now = DateTime.now();
+    //now.minute 으로 출력하면 03분으로 안나온다.
+    //그래서 DateTime을 포맷해서 출력한다.
+    final nowTime = DateFormat('HH:mm').format(now); 
+
+    _alarms.add(nowTime);
+    notifyListeners();// setState 했던것 처럼 화면 변화가 일어 난다.
+  }
+
+  void removeAlarm(String alarmTime){
+    _alarms.remove(alarmTime);
+    notifyListeners();
+  }
+}
